5fd5af81a244bf32e5c6f4c7a698594cde3ba850
iabloko <31312961+iabloko@users.noreply.github.com>
2018-10-26 18:02:22 +0300

    ColorEffect

---
+++ Learning/Assets/AnimatedLogo/Voxelizer/Scripts/colorBurst.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorBurst : MonoBehaviour {
+    public Voxelizer _voxelizer;
+    [SerializeField] private ReflectionProbe _reflectionProbe;
+    [SerializeField] private Texture hueTexture;
+    public GameObject _colorBurst;
+    private float colorHUE, dpiScale;
+    private GUIStyle guiStyleHeader = new GUIStyle ();
+    private Color guiColor = Color.red;
+
+    void Awake () {
+        if (!_reflectionProbe) {
+            Debug.LogError ("PLeaseAddReflectionProbe");
+
+        }
+    }
+    void Start () {
+        if (Screen.dpi < 1) dpiScale = 1;
+        if (Screen.dpi < 200) dpiScale = 1;
+        else dpiScale = Screen.dpi / 200f;
+        guiStyleHeader.fontSize = (int) (15f * dpiScale);
+        guiStyleHeader.normal.textColor = guiColor;
+    }
+
+    private void OnGUI () {
+        var offset = 0f;
+
+        GUI.Label (new Rect (400 * dpiScale, 15 * dpiScale + offset / 2, 100 * dpiScale, 20 * dpiScale),
+            "LogoAscreen \"");
+
+        GUI.DrawTexture (new Rect (12 * dpiScale, 140 * dpiScale + offset, 285 * dpiScale, 15 * dpiScale), hueTexture, ScaleMode.StretchToFill, false, 0);
+
+        float oldColorHUE = colorHUE;
+        colorHUE = GUI.HorizontalSlider (new Rect (12 * dpiScale, 147 * dpiScale + offset, 285 * dpiScale, 15 * dpiScale), colorHUE, 0, 360);
+
+        if (Mathf.Abs (oldColorHUE - colorHUE) > 0.001) {
+            _voxelizer._lineColor = ME_ColorHelper.ConvertRGBColorByHUE (_voxelizer._lineColor, colorHUE / 360f);
+        }
+
+    }
+}
---
+++ Learning/Assets/_Shaders/Common/Helpers/ME_ColorHelper.cs
@@ -0,0 +1,134 @@
+using System;
+using UnityEngine;
+
+public static class ME_ColorHelper
+{
+    const float TOLERANCE = 0.0001f;
+
+    public struct HSBColor
+    {
+        public float H;
+        public float S;
+        public float B;
+        public float A;
+
+        public HSBColor(float h, float s, float b, float a)
+        {
+            this.H = h;
+            this.S = s;
+            this.B = b;
+            this.A = a;
+        }
+    }
+
+    public static HSBColor ColorToHSV(Color color)
+    {
+        HSBColor ret = new HSBColor(0f, 0f, 0f, color.a);
+
+        float r = color.r;
+        float g = color.g;
+        float b = color.b;
+
+        float max = Mathf.Max(r, Mathf.Max(g, b));
+
+        if (max <= 0)
+            return ret;
+
+        float min = Mathf.Min(r, Mathf.Min(g, b));
+        float dif = max - min;
+
+        if (max > min)
+        {
+            if (Math.Abs(g - max) < TOLERANCE)
+                ret.H = (b - r)/dif*60f + 120f;
+            else if (Math.Abs(b - max) < TOLERANCE)
+                ret.H = (r - g)/dif*60f + 240f;
+            else if (b > g)
+                ret.H = (g - b)/dif*60f + 360f;
+            else
+                ret.H = (g - b)/dif*60f;
+            if (ret.H < 0)
+                ret.H = ret.H + 360f;
+        }
+        else
+            ret.H = 0;
+
+        ret.H *= 1f/360f;
+        ret.S = (dif/max)*1f;
+        ret.B = max;
+
+        return ret;
+    }
+
+    public static Color HSVToColor(HSBColor hsbColor)
+    {
+        float r = hsbColor.B;
+        float g = hsbColor.B;
+        float b = hsbColor.B;
+        if (Math.Abs(hsbColor.S) > TOLERANCE)
+        {
+            float max = hsbColor.B;
+            float dif = hsbColor.B*hsbColor.S;
+            float min = hsbColor.B - dif;
+
+            float h = hsbColor.H*360f;
+
+            if (h < 60f)
+            {
+                r = max;
+                g = h*dif/60f + min;
+                b = min;
+            }
+            else if (h < 120f)
+            {
+                r = -(h - 120f)*dif/60f + min;
+                g = max;
+                b = min;
+            }
+            else if (h < 180f)
+            {
+                r = min;
+                g = max;
+                b = (h - 120f)*dif/60f + min;
+            }
+            else if (h < 240f)
+            {
+                r = min;
+                g = -(h - 240f)*dif/60f + min;
+                b = max;
+            }
+            else if (h < 300f)
+            {
+                r = (h - 240f)*dif/60f + min;
+                g = min;
+                b = max;
+            }
+            else if (h <= 360f)
+            {
+                r = max;
+                g = min;
+                b = -(h - 360f)*dif/60 + min;
+            }
+            else
+            {
+                r = 0;
+                g = 0;
+                b = 0;
+            }
+        }
+
+        return new Color(Mathf.Clamp01(r), Mathf.Clamp01(g), Mathf.Clamp01(b), hsbColor.A);
+    }
+
+    public static Color ConvertRGBColorByHUE(Color rgbColor, float hue)
+    {
+        var brightness = ColorToHSV(rgbColor).B;
+        if (brightness < TOLERANCE)
+            brightness = TOLERANCE;
+        var hsv = ColorToHSV(rgbColor / brightness);
+        hsv.H = hue;
+        var color = HSVToColor(hsv) * brightness;
+        color.a = rgbColor.a;
+        return color;
+    }
+}
